5afd3898a166cb006ef37f6d19daf9457e118c46
zhixun04 <s10258774@connect.np.edu.sg>
2023-11-27 17:27:16 +0800

    send loginpage.js to github

--- Loginpage.js
+++ Loginpage.js
@@ -1,50 +1,53 @@
-// JavaScript for form validation
+function func(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email == 'peyzhixun@gmail.com' && password == '12345')
+    {
+        alert("Success")
+        window.location.assign("Home.html")
+    }
+   else
+   {
+        alert("Wrong entry")
+   }
+}
+
 function validateForm() { 
     var email = document.forms["loginForm"]["Email"].value;
     var password = document.forms["loginForm"]["Password"].value;
 
     if (email === "" && password === "") {
         alert("Please enter both email and password.");
+        
         return false;
     } else if (email === "") {
         alert("Please enter your email.");
+        
         return false;
     } else if (password === "") {
         alert("Please enter your password.");
+
         return false;
     }    
-    return true;
+    return false;
 }
 
-function checkUsernameLength() {
+function checkUsernameLength()
+{
     var username = document.getElementById("email").value;
     var feedback = document.getElementById("usernameFeedback");
-    if (username.length < 6) {
-        feedback.innerText = "Username should be at least 6 characters long.";
-    } else {
-        feedback.innerText = "Username is long enough"; 
+    if (username.length < 6)
+    {
+        feedback.innerText = "Username should be at least 6 characters.";
+    }
+    else
+    {
+        feedback.innerText = "Username is long enough.";
     }
 }
 
-function func() {
-    var email = document.getElementById("email").value;
+function checkPasswordValidity()
+{
     var password = document.getElementById("password").value;
-    if (email === 'peyzhixun@gmail.com' && password === '12345') {
-        alert("Success")
-        window.location.assign("Home.html")
-    } else {
-        alert("Wrong entry")
-    }
-}
-
-
-function func(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (email === 'peyzhixun@gmail.com' && password === '12345') {
-        alert("Success")
-        window.location.href = "Home.html"; // Correct way to set the href property
-    } else {
-        alert("Wrong entry")
-    }
+    
 }
