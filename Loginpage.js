1304f5656642ccd9a144e00982025c22d50de1e7
zhixun04 <s10258774@connect.np.edu.sg>
2023-11-28 14:02:51 +0800

    update Loginpage.js to github

--- Loginpage.js
+++ Loginpage.js
@@ -1,7 +1,7 @@
 function func(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    if (email == 'peyzhixun@gmail.com' && password == '12345')
+    if (email == 'peyzhixun@gmail.com' && password == 'Badminton2023')
     {
         alert("Success")
         window.location.assign("Home.html")
@@ -46,8 +46,36 @@ function checkUsernameLength()
     }
 }
 
-function checkPasswordValidity()
-{
-    var password = document.getElementById("password").value;
-    
+function validateForm() { 
+    var email = document.forms["loginForm"]["Email"].value;
+    var password = document.forms["loginForm"]["Password"].value;
+    var passwordFeedback = document.getElementById("passwordFeedback");
+
+    if (email === "" && password === "") {
+        alert("Please enter both email and password.");
+        return false;
+    } else if (email === "") {
+        alert("Please enter your email.");
+        return false;
+    } else if (password === "") {
+        alert("Please enter your password.");
+        return false;
+    } else {
+        if (!password.match(/[0-9]/)) {
+            // Password does not contain at least one number
+            passwordFeedback.textContent = "Password must contain at least one number.";
+            return false;
+            
+        }
+        if (!password.match(/[A-Za-z]/)) {
+            // Password does not contain at least one letter
+            passwordFeedback.textContent = "Password must contain at least one letter.";
+            return false;
+        }
+        // Clear the feedback when password is valid
+        passwordFeedback.textContent = "Password is strong.";
+        return false;
+    }
 }
+
+
